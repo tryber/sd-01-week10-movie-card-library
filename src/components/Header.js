37448406228bad11e70669ee3a3e17edddceb0f4
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-11-18 18:44:55 -0200

    Finished Work

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,8 @@ import React, { Component } from 'react';
 export default class Header extends Component {
     render() {
         return (
-            <header className="header">
-                <h1>Movie Cards</h1>
+            <header className="movie-card-header page-title">
+                <h1>Movie Cards Library</h1>
             </header>
         )
     }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,1 +1,17 @@
 import React, { Component } from 'react';
+import Rating from './Rating';
+
+export default class MovieCard extends Component {
+    render() {
+        const { title, subtitle, storyline, imagePath, rating } = this.props.movie;
+        return (
+            <article className="movie-card movie-card-body">
+                <img src={imagePath} className="movie-card-image"></img>
+                <h4 className="movie-card-title">{title}</h4>
+                <h5 className="movie-card-subtitle">{subtitle}</h5>
+                <p className="movie-card-storyline">{storyline}</p>
+                <Rating rating={rating}/>
+            </article>
+        )
+    }
+}
