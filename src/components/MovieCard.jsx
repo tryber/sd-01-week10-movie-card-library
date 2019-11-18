a041b04efa06377aa078e7c6f681d7e38bcff1a7
antoniopaolillo <antonio_francisco_p@live.com>
2019-11-18 15:52:36 -0300

    finalizando requisitos

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import Rating from './Rating';
+import '../App.css';
 
 class MovieCard extends React.Component {
     render() {
         const {title, subtitle, storyline, imagePath, rating} = this.props.movie;
-        return <article>
-        <img src={imagePath}></img>
-        <h4>{title}</h4>
-        <h5>{subtitle}</h5>
+        return <article className="movie-card movie-card-body">
+        <img className="movie-card-image" src={imagePath} alt=""></img>
+        <h4 className="movie-card-title">{title}</h4>
+        <h5 className="movie-card-subtitle">{subtitle}</h5>
+        <p className="movie-card-storyline">{storyline}</p>
+        <p className="movie-card-rating"><Rating className = "rating" rating={rating}/></p>
         </article>
-
     }
 }
 
---
+++ src/components/Rating.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class rating extends React.Component {
+    render() {
+        return this.props.rating;
+    }
+}
+
+export default rating;
