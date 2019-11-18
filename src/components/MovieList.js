b88d735cccc9c1bf04b029837f52a5bcdaaa4ee3
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-11-18 19:04:25 -0200

    Code Climate

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import MovieCard from './MovieCard.js';
 
-
-
 export default class MovieList extends Component {
-    render() {
-        const { movies } = this.props;
-        return (
-            <section className="movie-list">
-                {movies.map((element) => 
-                    <MovieCard key={element.title} movie={element}/>
-                )}
-            </section>
-        )
-    }
+  render() {
+    const { movies } = this.props;
+    return (
+      <section className="movie-list">
+        {movies.map((element) => <MovieCard key={element.title} movie={element} />
+        )}
+      </section>
+    );
+  }
 }
