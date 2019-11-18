3b8285a7f16187593432c33469dbd7c502d22e09
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-11-18 19:06:14 -0300

    Adicionando alterações

---
+++ src/components/MovieList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MovieCard from './MovieCard';
+
+class MovieList extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <div className="movie-list">
+        {movies.map((movie) => <MovieCard movie={movie} key={movie.title} />)}
+      </div>
+    );
+  }
+}
+
+export default MovieList;
