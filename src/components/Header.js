0e9b41ff279a0c2120c60fe6e606a8a21cb6bc66
Anderson Bolivar <bolivar.anderson@hotmail.com>
2019-11-18 16:42:38 -0200

    Add Header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Header extends Component {
+    render() {
+        return (
+            <header className="header">
+                <h1>Movie Cards</h1>
+            </header>
+        )
+    }
+}
