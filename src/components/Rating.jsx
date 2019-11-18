ea46baedddc2122b2d354cd9953c0072daf04630
Gabriel Dalseco dos Santos <gabriel_santos96@outlook.com>
2019-11-18 19:13:22 -0300

    adicionando alterações do codeclimate

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Rating extends React.Component {
   render() {
-    const rating = this.props.rating;
+    const { rating } = this.props.rating;
     return (
       <div className="movie-card-rating">
         <span className="rating">{rating}</span>
