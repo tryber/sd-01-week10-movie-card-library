cb2146110bcd28ec308313571e770f640972d3d4
Conrado Medeiros <conradodemedeiros@hotmail.com>
2019-11-19 12:48:16 -0300

    code cliamte

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './App.css';
-import data from './data'
-import Header from './components/Header'
-import MovieList from './components/MovieList'
+import data from './data';
+import Header from './components/Header';
+import MovieList from './components/MovieList';
 
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <MovieList movies = {data} />
+      <MovieList movies={data} />
     </div>
   );
 }
