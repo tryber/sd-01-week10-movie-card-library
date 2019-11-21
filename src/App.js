05e413a8fcefbdb01b2b23ed9b643b0a2654a296
Victor Felipe <vf.ramos7@gmail.com>
2019-11-21 16:45:07 -0300

    CodeClimate VI

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
 import './App.css';
-import movies from './data'
+import movies from './data';
 
 function App() {
   return (
